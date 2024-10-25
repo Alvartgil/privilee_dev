6ea6eb92fad60d86386c59650e341c0a13228191
Álvaro Artigas <aar@privilee.ae>
2024-10-25 06:18:57 +0000

    - stg_venue model added

--- definitions/UAE/sources/sources.js
+++ definitions/UAE/sources/sources.js
@@ -4,3 +4,9 @@ declare({
   name: "register",
   description: "Raw register data from MongoDB"
 });
+declare({
+  database: "privilee-12177", 
+  schema: "mongodb_production",
+  name: "venue",
+  description: "Raw register data from MongoDB"
+});
